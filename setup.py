29824bf1bf70ca28d6a5dee35685493940bae874
bichanna <nobu.bichanna@gmail.com>
2021-11-30 21:02:00 -0600

    Modify setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ from setuptools import setup, find_packages
 VERSION = '0.1.0' 
 DESCRIPTION = 'DNA proofreading'
 LONG_DESCRIPTION = """
-[Usage](https://github.com/bichanna/Proofreader)
+[Usage](https://github.com/bichanna/Proofreader-polymerase)
 """
 
 # Setting up
 setup(
-    name="DNAProofreader",
+    name="Proofreader-polymerase",
     version=VERSION,
     author="bichanna",
     author_email="nobu.bichanna@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/bichanna/Proofreader",
+    url="https://github.com/bichanna/Proofreader-polymerase",
     packages=find_packages(),
 	license="MIT",
-    keywords=["DNA", "proofread", "proofreading", "proofreader", "python"],
+    keywords=["DNA", "proofread", "proofreading", "proofreader", "python", "polymerase"],
     classifiers= [
         "Intended Audience :: Education"
     ],
     entry_points = """
         [console_scripts]
-        proofread=Proofreader.main:main
+        proofreader=Proofreader.main:main
     """
 )
